73c03e46defebbfbbc6fcc0d4b769a1b84a0bafb
zinx | Tasdid <tasdid110@gmail.com>
2023-10-08 14:57:29 +0600

    upload class progress

---
+++ LAB-5.2[PHP-Session]/index.php
@@ -0,0 +1,56 @@
+<?php
+    session_start();
+    if(isset($_SESSION['auth'])){
+        header('location: dashboard.php');
+    }
+?>
+
+<html lang="en">
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <table width="100%" height="100%" >
+        <tr height="40px">
+                <header>
+                <td>
+                   <h1>X Company</h1> 
+                </td>
+                <td align="right">
+                    <a href="index.php">Home</a>|
+                    <a href="login.php">Login</a>|
+                    <a href="registration.php">Registration</a>
+                </td>
+            
+                </td>
+            </header>
+        </tr>
+        <tr>
+            <td colspan="2" height="2px">
+
+                <hr>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+
+                
+                <main height="">
+                   <b> Welcome to xCompany</b>
+                </main>
+            </td>
+        </tr>
+        <tr  height="40px">
+            <td colspan="2">
+
+                
+                <hr>
+                <footer align="center">
+                    Copyright &copy; 2017
+                </footer>
+            </td>
+        </tr>
+    </table>
+    
+</body>
+</html>
